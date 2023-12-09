235150c544115861975c2635442795feca864b90
Roi-Yona <roiyona18@gmail.com>
2023-12-09 18:54:05 +0200

    Implement restrict TGD constraint
    Implement restrict TGD constraint,
    Adding a new module of DB server interface.
    This commit made after long time that I didn't touched the project.
    Therefore, the test of the restrict TGD might need to recheck (although
    the tests pass).

--- database_constraints_to_cnf_dnf_nnf/database_constraints_to_cnf.py
+++ database_constraints_to_cnf_dnf_nnf/database_constraints_to_cnf.py
@@ -1,3 +1,5 @@
+from sqlalchemy.engine import Engine
+import database_server_interface.database_server_interface as db_interface
 import pandas as pd
 import numpy as np
 import itertools
@@ -14,40 +16,35 @@ class DatabaseConstraintsConvertor:
        General Denial Constraint,
        General TGD.
     """
-    def __init__(self, database_dictionary: dict, number_of_voters=0, number_of_candidates=0,
+    def __init__(self, database_engine: Engine, number_of_voters=0, number_of_candidates=0,
                  candidates_column_name='id', voters_column_name='userId'):
         """Initializing the convertor.
 
-        :param database_dictionary:    key - the relation name, value - the relation path.
+        :param database_engine:        The input database engine (used for running database query).
         :param candidates_column_name: The name of the candidate column
                                        (needs to be the same in all the DB relations).
         :param number_of_voters:       The number of voters in the Approval Based Committee setting.
         :param number_of_candidates:   The number of candidates for the committee.
         """
         # Initializing
+        self._db_engine = database_engine
         self._number_of_candidates = number_of_candidates
         self._number_of_voters = number_of_voters
         self._candidates_column_name = candidates_column_name
         self._voters_column_name = voters_column_name
-
-        # Create the database
-        self._database = {}
-        for k, v in database_dictionary.items():
-            # TODO: Remove the 1000 limitation.
-            self._database[k] = pd.read_csv(v,  low_memory=False, nrows=1000)
 
     def convert(self, *args):
         pass
 
 
 class DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(DatabaseConstraintsConvertor):
-    """A Restrict Denial Constraint to CNF formula convertor"""
+    """A restricted Denial Constraint (Definition 10) to CNF formula convertor"""
 
     def __init__(self, *args):
         super().__init__(*args)
 
     def convert(self, relation_name: str, candidate_column: str, element_column: str):
-        """Create CNF formula, given a Restricted Denial Constraint.
+        """Create CNF formula, given a restricted Denial Constraint.
 
             Group according element column.
             For each group, demand that for each different Ci, Cj (id's from the candidate column) -
@@ -64,9 +61,16 @@ class DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(DatabaseConstrai
         # Contains the element name as key and list of candidates id's that in a relation with this element as value.
         candidates_ids_dict = {}
 
-        grouped_by_element_column = self._database[relation_name].groupby(element_column)
+        # Select the relevant columns from the database, and group by the element column.
+        sql_query = f"SELECT DISTINCT {candidate_column}, {element_column} " \
+                    f"FROM {relation_name} " \
+                    f"ORDER BY {element_column};"
+
+        # Group by element column and find all the candidates conflict for an element.
+        grouped_by_element_column = db_interface.database_run_query(self._db_engine, sql_query)
+        grouped_by_element_column = grouped_by_element_column.groupby(element_column)
         for element_name, element_df in grouped_by_element_column:
-            candidates_ids_dict[element_name] = element_df['id']
+            candidates_ids_dict[element_name] = element_df[candidate_column]
 
         # Convert the dictionary values to separate lists.
         candidates_ids_lists = list(candidates_ids_dict.values())
@@ -74,7 +78,7 @@ class DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(DatabaseConstrai
         # DEBUG: Print the split lists.
         if DEBUG_MODE:
             for idx, lst in enumerate(candidates_ids_lists):
-                print(f"DEBUG: Denial Constraint ID List {idx + 1}: {lst}")
+                print(f"DEBUG: Restrict Denial Constraint ID List {idx + 1}: {lst}")
 
         # Generate all possible two-item combinations from the list.
         for lst in candidates_ids_lists:
@@ -87,6 +91,92 @@ class DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(DatabaseConstrai
         cnf_string = f"p cnf {str(number_of_variables)} {str(number_of_clauses)}\n" + cnf_string
 
         if DEBUG_MODE:
-            print(f"DEBUG: Denial Constraint CNF string: {cnf_string}")
+            print(f"DEBUG: Restrict Denial Constraint CNF string: {cnf_string}")
 
         return cnf_string
+
+
+class DatabaseConstraintsConvertorRestrictTGDConstraintToCNF(DatabaseConstraintsConvertor):
+    """A restricted Tuple-Generating Dependency (TGD) Constraint (Definition 7) to CNF formula convertor."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def convert(self, relations_list: list, candidates_column_name: str):
+        """Create CNF formula, given a restricted TGD Constraint.
+
+            For each i index in the relations list -
+            Join Ri, Si by element column.
+            For each element group, save the group of candidates.
+            Demand 'or' between all candidates of a  group.
+
+            :param relations_list:         The input relations list for the constraint.
+                                           Each item in the list is tuple of
+                                           (R_i - relation name, S_i - relation name,
+                                           element column name)
+            :param candidates_column_name: The candidates column name for the S_i relations.
+        """
+        R_i_RELATION_NAME = 0
+        S_i_RELATION_NAME = 1
+        ELEMENT_COLUMN_NAME = 2
+
+        number_of_clauses = 0
+        number_of_variables = self._number_of_candidates
+        cnf_string = ''
+
+        # Contains the element name as key and list of candidates id's that in a relation with this element as value.
+        candidates_ids_dict = {}
+
+        for relation in relations_list:
+            # Select the relevant columns from the database, join and group by the element column.
+            sql_query = f"SELECT DISTINCT t2.{candidates_column_name}, t1.{relation[ELEMENT_COLUMN_NAME]} " \
+                        f"FROM {relation[R_i_RELATION_NAME]} t1 " \
+                        f"LEFT JOIN {relation[S_i_RELATION_NAME]} t2 " \
+                        f"on t1.{relation[ELEMENT_COLUMN_NAME]} = t2.{relation[ELEMENT_COLUMN_NAME]} "\
+                        f"ORDER BY t1.{relation[ELEMENT_COLUMN_NAME]};"
+            joined_R_S_i = db_interface.database_run_query(self._db_engine, sql_query)
+
+            grouped_by_element_column = joined_R_S_i.groupby(relation[ELEMENT_COLUMN_NAME])
+            for element_name, element_df in grouped_by_element_column:
+                candidates_ids_dict[element_name] = element_df[candidates_column_name]
+
+        # Convert the dictionary values to separate lists.
+        candidates_ids_lists = list(candidates_ids_dict.values())
+
+        # DEBUG: Print the split lists.
+        if DEBUG_MODE:
+            for idx, lst in enumerate(candidates_ids_lists):
+                print(f"DEBUG: Restrict TGD ID List {idx + 1}: {lst}")
+
+        # Add the restrictions to the CNF clause.
+        for candidates_ids_list in candidates_ids_lists:
+            for candidate in candidates_ids_list:
+                if not pd.isna(candidate):
+                    cnf_string += f"{str(int(candidate))} "
+            cnf_string += "0\n"
+            number_of_clauses += 1
+
+        cnf_string = f"p cnf {str(number_of_variables)} {str(number_of_clauses)}\n" + cnf_string
+
+        if DEBUG_MODE:
+            print(f"DEBUG: Restrict TGD Constraint CNF string: {cnf_string}")
+
+        return cnf_string
+
+
+if __name__ == '__main__':
+    server = 'LAPTOP-MO1JPG72'
+    database = 'the_basketball_synthetic_db'
+    db_engine = db_interface.database_connect(server, database)
+
+    # Sanity test.
+    print('\nDatabaseConstraintsConvertorRestrictDenialConstraintToCNF - Start Testing:')
+    dbc_to_cnf_tester = DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(db_engine, 0, 10)
+    cnf_string_predicted_outcome = 'p cnf 10 6\n-1 -2 0\n-1 -7 0\n-2 -7 0\n-3 -5 0\n-3 -10 0\n-5 -10 0\n'
+    cnf_string = dbc_to_cnf_tester.convert('basketball_metadata', 'id', 'position')
+    if cnf_string_predicted_outcome != cnf_string:
+        print("********************")
+        print("Failed!")
+        print("********************")
+    print(f"CNF convert results:\n{cnf_string}")
+    print('DatabaseConstraintsConvertorRestrictDenialConstraintToCNF - Test completed successfully.')
--- tests/database_constraints_to_cnf_dnf_nnf_tests.py
+++ tests/database_constraints_to_cnf_dnf_nnf_tests.py
@@ -1,41 +1,41 @@
+import database_server_interface.database_server_interface as db_interface
 import unittest
 import pathlib
-import numpy as np
 import pandas as pd
+import numpy as np
 
 import database_constraints_to_cnf_dnf_nnf.database_constraints_to_cnf as dbc_to_cnf
 
-# Test running instructions:
-# python -m unittest .\tests\database_constraints_to_cnf_dnf_nnf_tests.py
 CNF_RESULTS_PATH = '.\\CNF_results'
 
 
 class TestDatabaseConstraintsConvertor(unittest.TestCase):
-    MOVIES_DATABASE_PATH = ".\\databases\\the_movies_database\\"
-    _movies_database_dictionary = {'movies_rating': pathlib.Path(MOVIES_DATABASE_PATH + 'ratings.csv'),
-                                  'movies_metadata': pathlib.Path(MOVIES_DATABASE_PATH + 'movies_metadata.csv')}
-    _movies_number_of_candidates = pd.read_csv(_movies_database_dictionary['movies_metadata'], low_memory=False)['id'].astype(int).max()
-    _movies_number_of_voters = pd.read_csv(_movies_database_dictionary['movies_rating'], low_memory=False)['userId'].max()
-    print(f"LOG: The number of voters in the movies database is: {_movies_number_of_voters}")
-    print(f"LOG: The number of candidates (movies) in the movies database is: {_movies_number_of_candidates}")
+    # Set the movie's database.
+    # MOVIES_DATABASE_PATH = ".\\databases\\the_movies_database\\"
+    # _movies_database_dictionary = {'movies_rating': pathlib.Path(MOVIES_DATABASE_PATH + 'ratings.csv'),
+    #                                'movies_metadata': pathlib.Path(MOVIES_DATABASE_PATH + 'movies_metadata.csv')}
+    # _movies_number_of_candidates = pd.read_csv(_movies_database_dictionary['movies_metadata'],
+    #                                            low_memory=False)['id'].astype(int).max()
+    # _movies_number_of_voters = pd.read_csv(_movies_database_dictionary['movies_rating'],
+    #                                        low_memory=False)['userId'].\
+    #     max()
+    # print(f"LOG: The number of voters in the movies database is: {_movies_number_of_voters}")
+    # print(f"LOG: The number of candidates (movies) in the movies database is: {_movies_number_of_candidates}")
 
-    BASKETBALL_DATABASE_PATH = ".\\databases\\the_basketball_database\\"
-    _basketball_database_dictionary = {'basketball_metadata': pathlib.Path(BASKETBALL_DATABASE_PATH + 'basketball_metadata.csv')}
-    _basketball_number_of_candidates = pd.read_csv(_basketball_database_dictionary['basketball_metadata'], low_memory=False)['id'].max()
+    server = 'LAPTOP-MO1JPG72'
+
+    # Set the synthetic basketball database.
+    _basketball_database_engine = db_interface.database_connect(server, 'the_basketball_synthetic_db')
     _basketball_number_of_voters = 0
+    _basketball_number_of_candidates = db_interface.database_run_query(_basketball_database_engine,
+                                                                       "SELECT MAX(id) "
+                                                                       "FROM basketball_metadata;").iloc[0, 0]
     print(f"LOG: The number of candidates (players) in the basketball database is: {_basketball_number_of_candidates}")
 
     def test_db_constraints_convertor_restrict_denial_constraint_to_cnf(self):
         print('\nDatabaseConstraintsConvertorRestrictDenialConstraintToCNF - Start Testing:')
-
-        # dbc_to_cnf_tester = dbc_to_cnf.\
-        #     DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(self._movies_database_dictionary,
-        #                                                               self._movies_number_of_voters,
-        #                                                               self._movies_number_of_candidates)
-        # dbc_to_cnf_tester.convert('movies_metadata', 'id', 'production_companies')
-
         dbc_to_cnf_tester = dbc_to_cnf. \
-            DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(self._basketball_database_dictionary,
+            DatabaseConstraintsConvertorRestrictDenialConstraintToCNF(self._basketball_database_engine,
                                                                       self._basketball_number_of_voters,
                                                                       self._basketball_number_of_candidates)
         cnf_string_predicted_outcome = 'p cnf 10 6\n-1 -2 0\n-1 -7 0\n-2 -7 0\n-3 -5 0\n-3 -10 0\n-5 -10 0\n'
@@ -46,22 +46,29 @@ class TestDatabaseConstraintsConvertor(unittest.TestCase):
         with open(pathlib.Path(f"{CNF_RESULTS_PATH}\\RestrictDenialConstraintTest.txt"), 'w') as file:
             file.write(cnf_string)
         print(f"CNF convert results:\n{cnf_string}")
-
         print('DatabaseConstraintsConvertorRestrictDenialConstraintToCNF - Test completed successfully.')
 
-    # def test_isupper(self):
-    #     self.assertEqual('foo'.upper(), 'FOO')
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-    #
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    def test_db_constraints_convertor_restrict_tgd_constraint_to_cnf(self):
+        print('\nDatabaseConstraintsConvertorRestrictTGDConstraintToCNF - Start Testing:')
+
+        dbc_to_cnf_tester = dbc_to_cnf. \
+            DatabaseConstraintsConvertorRestrictTGDConstraintToCNF(self._basketball_database_engine,
+                                                                   self._basketball_number_of_voters,
+                                                                   self._basketball_number_of_candidates)
+        cnf_string_predicted_outcome = 'p cnf 10 6\n2 3 6 7 0\n3 5 8 0\n8 0\n0\n4 0\n9 10 0\n'
+        cnf_string = dbc_to_cnf_tester.convert([('basketball_metadata', 'basketball_metadata2', 'position')], 'userId')
+        self.assertEqual(cnf_string, cnf_string_predicted_outcome)
+
+        # Write compilation results to a file.
+        with open(pathlib.Path(f"{CNF_RESULTS_PATH}\\RestrictTGDTest.txt"), 'w') as file:
+            file.write(cnf_string)
+        print(f"CNF convert results:\n{cnf_string}")
+
+        print('DatabaseConstraintsConvertorRestrictTGDToCNF - Test completed successfully.')
 
 
+# Test running instructions:
+# python -m unittest .\tests\database_constraints_to_cnf_dnf_nnf_tests.py
 if __name__ == '__main__':
     print('Starting...')
     # Create a test suite
