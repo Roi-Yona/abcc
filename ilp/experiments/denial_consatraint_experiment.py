b7b918bd0215e51d9e41c1a5b9be19fe515714b3
Roi-Yona <roiyona18@gmail.com>
2024-01-27 17:19:04 +0200

    Implement denial constraint experiment

---
+++ ilp/experiments/denial_consatraint_experiment.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pathlib
+
+import config
+import ilp.ilp_reduction.thiele_rule_to_ilp.thiele_functions as thiele_functions
+import ilp.ilp_db_data_extractors.thiele_rule_db_data_extractor as thiele_rule_db_data_extractor
+import ilp.ilp_db_data_extractors.denial_constraint_db_data_extractor as denial_constraint_extractor
+import experiment
+
+MODULE_NAME = "Denial Constraint Experiment"
+START_EXPERIMENT_RANGE = 5000
+END_EXPERIMENT_RANGE = 5001#100001
+TICK_EXPERIMENT_RANGE = 5000
+
+
+class DenialConstraintExperiment(experiment.Experiment):
+    def __init__(self,
+                 experiment_name: str, database_name: str,
+                 solver_time_limit: int, solver_name: str,
+                 denial_constraint_dict: dict, committee_members_list: list, candidates_tables: list,
+                 committee_size: int, voters_size_limit: int, candidates_size_limit: int,
+                 thiele_rule_function_creator,
+                 voting_table_name='voting',
+                 candidates_table_name='candidates',
+                 candidates_column_name='candidate_id',
+                 voters_column_name='voter_id',
+                 approval_column_name='rating',
+                 ):
+        super().__init__(experiment_name, database_name, solver_time_limit, solver_name)
+
+        self._voters_group_size = voters_size_limit
+        self._candidates_group_size = candidates_size_limit
+        self._committee_size = committee_size
+
+        # Create the data extractors.
+
+        # NOTE_1: Can alternative define here a denial constraint data after extracting directly using SQL query.
+        self._denial_constraint_db_extractor = denial_constraint_extractor.DenialConstraintDBDataExtractor(
+            self._abc_convertor, self._db_engine, denial_constraint_dict, committee_members_list, candidates_tables,
+            committee_size, voters_size_limit, candidates_size_limit, candidates_column_name, voters_column_name)
+
+        self._av_db_data_extractor = thiele_rule_db_data_extractor.ThieleRuleDBDataExtractor(
+            self._abc_convertor, self._db_engine,
+            committee_size, voters_size_limit, candidates_size_limit,
+            thiele_rule_function_creator(committee_size + 1),
+            voting_table_name, candidates_table_name, candidates_column_name, voters_column_name, approval_column_name)
+
+    @staticmethod
+    def create_results_df() -> pd.DataFrame:
+        experiments_results = {
+            'voters_group_size': [],
+            'candidates_group_size': [],
+            'committee_size': [],
+            'solving_time(sec)': []
+        }
+        return pd.DataFrame(experiments_results)
+
+    def run_experiment(self):
+        # Extract problem data from the database and convert to ILP.
+        # NOTE_1: Can alternative convert to ilp directly using the _abc_convertor using a data that already extracted.
+        self._denial_constraint_db_extractor.extract_and_convert()
+        self._av_db_data_extractor.extract_and_convert()
+
+        # Run the experiment.
+        solved_time = self.run_model()
+
+        # Save the results.
+        new_result = {'voters_group_size': self._voters_group_size,
+                      'candidates_group_size': self._candidates_group_size,
+                      'committee_size': self._committee_size,
+                      'solving_time(sec)': solved_time}
+        return pd.DataFrame([new_result])
+
+
+# Functions------------------------------------------------------------------
+def thiele_rule_experiment_save_graph(df: pd.DataFrame, experiment_name: str, results_file_path: str):
+    config.debug_print(MODULE_NAME, f"Experiment results\n{str(df)}")
+    result_path = f'{results_file_path}{experiment_name}.png'
+    result_path = pathlib.Path(result_path)
+
+    # Scatter plot.
+    plt.scatter(df['voters_group_size'], df['solving_time(sec)'])
+    # Set specific tick marks.
+    plt.xticks(range(START_EXPERIMENT_RANGE, END_EXPERIMENT_RANGE, TICK_EXPERIMENT_RANGE))
+    # Set axis labels.
+    plt.xlabel('voters_group_size')
+    plt.ylabel('solving_time(sec)')
+    # Set plot title.
+    plt.title(f"candidates_group_size = {df['voters_group_size'][0]}, committee_size = {df['committee_size'][0]}")
+    # Save the plot
+    plt.savefig(result_path)
+
+
+def denial_constraint_experiment_runner(experiment_name: str, database_name: str,
+                                        solver_time_limit: int,
+                                        solver_name: str,
+                                        denial_constraint_dict: dict, committee_members_list: list,
+                                        candidates_tables: list,
+                                        committee_size: int,
+                                        candidates_size_limit: int,
+                                        thiele_rule_function_creator,
+                                        voting_table_name: str,
+                                        ):
+    experiments_results = DenialConstraintExperiment.create_results_df()
+
+    for voters_size_limit in range(START_EXPERIMENT_RANGE, END_EXPERIMENT_RANGE, TICK_EXPERIMENT_RANGE):
+        config.debug_print(MODULE_NAME, f"voters_group_size={voters_size_limit}\n"
+                                        f"candidates_group_size={candidates_size_limit}\n"
+                                        f"committee_size={committee_size}")
+        cc_experiment = DenialConstraintExperiment(experiment_name, database_name,
+                                                   solver_time_limit, solver_name,
+                                                   denial_constraint_dict, committee_members_list, candidates_tables,
+                                                   committee_size, voters_size_limit, candidates_size_limit,
+                                                   thiele_rule_function_creator,
+                                                   voting_table_name)
+        experiments_results = experiment.save_result(experiments_results, cc_experiment.run_experiment())
+        experiment.experiment_save_excel(experiments_results, experiment_name, cc_experiment.results_file_path)
+
+
+if __name__ == '__main__':
+    # Experiments----------------------------------------------------------------
+    # # CC Thiele Rule - the_movies_database - Without Loop------------------------
+    # _experiment_name = 'CC Thiele Rule AND Denial Constraint'
+    # _database_name = 'the_movies_database'
+    # _solver_time_limit = 1
+    # _solver_name = "SAT"
+    #
+    # # 10000 succeed, 100000 didn't status = 6 - model is invalid
+    # _voters_size_limit = 30000
+    # _candidates_size_limit = 30
+    # _committee_size = 10
+    # _thiele_rule_function_creator = thiele_functions.create_cc_thiele_dict
+    #
+    # _voting_table_name = 'voting'
+    #
+    # _denial_constraint_dict = dict()
+    # _denial_constraint_dict[('candidates', 't1')] = [('c1', 'candidate_id'), ('x', 'genres')]
+    # _denial_constraint_dict[('candidates', 't2')] = [('c2', 'candidate_id'), ('x', 'genres')]
+    # _committee_members_list = ['c1', 'c2']
+    # _candidates_tables = ['t1', 't2']
+    #
+    # _experiments_results = DenialConstraintExperiment.create_results_df()
+    # cc_experiment = DenialConstraintExperiment(_experiment_name, _database_name,
+    #                                            _solver_time_limit, _solver_name,
+    #                                            _denial_constraint_dict, _committee_members_list, _candidates_tables,
+    #                                            _committee_size, _voters_size_limit, _candidates_size_limit,
+    #                                            _thiele_rule_function_creator,
+    #                                            _voting_table_name)
+    # experiment.save_result(_experiments_results, cc_experiment.run_experiment())
+
+    # CC Thiele Rule - the_movies_database---------------------------------------
+    # Define the experiment.
+    _experiment_name = 'CC Thiele Rule AND Denial Constraint'
+    _database_name = 'the_movies_database'
+    _solver_time_limit = 270
+    _solver_name = "SAT"
+
+    _candidates_size_limit = 30
+    _committee_size = 10
+    _thiele_rule_function_creator = thiele_functions.create_cc_thiele_dict
+
+    _voting_table_name = 'voting'
+
+    _denial_constraint_dict = dict()
+    _denial_constraint_dict[('candidates', 't1')] = [('c1', 'candidate_id'), ('x', 'genres')]
+    _denial_constraint_dict[('candidates', 't2')] = [('c2', 'candidate_id'), ('x', 'genres')]
+    _committee_members_list = ['c1', 'c2']
+    _candidates_tables = ['t1', 't2']
+
+    # Run the experiment.
+    denial_constraint_experiment_runner(_experiment_name, _database_name,
+                                        _solver_time_limit,
+                                        _solver_name,
+                                        _denial_constraint_dict, _committee_members_list, _candidates_tables,
+                                        _committee_size, _candidates_size_limit,
+                                        _thiele_rule_function_creator,
+                                        _voting_table_name)
---
+++ ilp/ilp_db_data_extractors/denial_constraint_db_data_extractor.py
@@ -0,0 +1,138 @@
+"""A class for converting an ABC contextual constraint - denial constraint
+   to an ILP constraint.
+
+       The problem original input is a logic formula of the form of denial constraint:
+        For All c_i_1,...,c_i_l,x_j_1,...,x_j_l':
+         not (R_h_1(...) AND ... AND R_h_l''(...) AND Committee(c_i_1) AND ... AND Committee(c_i_l))
+
+    relations_dict             Represents theta in the denial constraint, for example -
+                               relations_dict = {('R1', 'R1_a'): [('x', 'x_real_R1_name'), ('y','y_real_R1_name'),...,],...}.
+    committee_candidates_list  Represents theta' in the denial constraint, for example -
+                               committee_candidates_list = ['y', 'z'].
+"""
+import pandas as pd
+
+import config
+from sqlalchemy.engine import Engine
+from database.database_server_interface import database_server_interface as db_interface
+import ilp.ilp_reduction.abc_to_ilp_convertor as ilp_convertor
+import ilp.ilp_db_data_extractors.db_data_extractor as db_data_extractor
+
+MODULE_NAME = "Denial Constraint DB Data Extractor"
+
+
+class DenialConstraintDBDataExtractor(db_data_extractor.DBDataExtractor):
+    def __init__(self,
+                 abc_convertor: ilp_convertor.ABCToILPConvertor,
+                 database_engine: db_interface.Database,
+                 denial_constraint_dict: dict,
+                 committee_members_list: list,
+                 candidates_tables: list,
+                 committee_size: int,
+                 voters_size_limit: int,
+                 candidates_size_limit: int,
+                 candidates_column_name='candidate_id',
+                 voters_column_name='voter_id',
+                 ):
+
+        super().__init__(abc_convertor, database_engine)
+
+        self._committee_size = committee_size
+        self._voters_size_limit = voters_size_limit
+        self._candidates_size_limit = candidates_size_limit
+        self._candidates_column_name = candidates_column_name
+        self._voters_column_name = voters_column_name
+
+        self._denial_constraint_dict = denial_constraint_dict
+        self._committee_members_list = committee_members_list
+        self._candidates_tables = candidates_tables
+        self._denial_constraint_candidates_df = None
+
+    def join_tables(self, tables_dict: dict) -> pd.DataFrame:
+        """Extract the DB join between all the tables in the denial constraint dict.
+        When there are shared columns join natural inner join, otherwise, cross join.
+
+        :param tables_dict:
+        :return: The resulted database.
+        """
+
+        variables_dict = dict()
+        for table_name_tuple, variables in tables_dict.items():
+            for var in variables:
+                if var[0] not in variables_dict:
+                    variables_dict[var[0]] = []
+                    variables_dict[var[0]].append(table_name_tuple[1])
+                else:
+                    variables_dict[var[0]].append(table_name_tuple[1])
+
+        # Create From phrase.
+        from_phrase = 'FROM '
+        for table_name_tuple in tables_dict:
+            from_phrase += f"{table_name_tuple[0]} AS {table_name_tuple[1]}, "
+        from_phrase = from_phrase[:len(from_phrase) - 2]
+        from_phrase += '\n'
+
+        # Create Select phrase.
+        select_phrase = 'SELECT '
+        for var, table_names in variables_dict.items():
+            table_var_name = 'None'
+            for x, y in tables_dict.items():
+                if x[1] == table_names[0]:
+                    for z in y:
+                        if z[0] == var:
+                            table_var_name = z[1]
+                            break
+            select_phrase += f"{table_names[0]}.{table_var_name} AS {var}, "
+        select_phrase = select_phrase[:len(select_phrase) - 2]
+        select_phrase += '\n'
+
+        # Create  Where phrase.
+        where_phrase = 'WHERE '
+        for var, table_names in variables_dict.items():
+            for i in range(0, len(table_names) - 1, 2):
+                table_var_name = 'None'
+                for x, y in tables_dict.items():
+                    if x[1] == table_names[i]:
+                        for z in y:
+                            if z[0] == var:
+                                table_var_name = z[1]
+                                break
+                table_var_name2 = 'None'
+                for x, y in tables_dict.items():
+                    if x[1] == table_names[i + 1]:
+                        for z in y:
+                            if z[0] == var:
+                                table_var_name2 = z[1]
+                                break
+                if i == len(table_names) - 2:
+                    where_phrase += f"{table_names[i]}.{table_var_name} = " \
+                                    f"{table_names[i + 1]}.{table_var_name2}"
+                else:
+                    where_phrase += f"{table_names[i]}.{table_var_name} = " \
+                                    f"{table_names[i + 1]}.{table_var_name2} AND "
+        for table_name in self._candidates_tables:
+            where_phrase += f" AND {table_name}.{self._candidates_column_name} <= {self._candidates_size_limit}"
+        where_phrase += '\n'
+        config.debug_print(MODULE_NAME,
+                           "The extract data SQL phrase is: \n" + select_phrase + from_phrase + where_phrase)
+        legal_assignments = self._db_engine.run_query(select_phrase + from_phrase + where_phrase)
+        # legal_assignments = db_interface.database_run_query(self._db_engine,
+        #                                                     select_phrase + from_phrase + where_phrase)
+        return legal_assignments
+
+    def extract_data_from_db(self) -> None:
+        legal_assignments = self.join_tables(self._denial_constraint_dict)
+
+        # Extract the committee members sets out of the resulted join.
+        self._denial_constraint_candidates_df = legal_assignments[self._committee_members_list]
+
+        config.debug_print(MODULE_NAME,
+                           f"The denial constraints candidates are: {self._denial_constraint_candidates_df}.")
+
+    def convert_to_ilp(self) -> None:
+        self._abc_convertor.define_denial_constraint(
+            self._denial_constraint_candidates_df)
+
+
+if __name__ == '__main__':
+    pass
