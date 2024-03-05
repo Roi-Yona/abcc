6f3007bb2fb229fd07ad99697835fee61b8f6de3
Roi-Yona <roiyona18@gmail.com>
2024-03-06 00:57:33 +0200

    Update candidate start point of experiments

--- ilp/experiments/experiment_22/thiele_rule_CC_no_constraint_optimization_size_30_10_SAT_solver_different_starting_point.py
+++ ilp/experiments/experiment_22/thiele_rule_CC_no_constraint_optimization_size_30_10_SAT_solver_different_starting_point.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     _candidates_size_limit = 30
     _committee_size = 10
     _voters_starting_point = 0
-    _candidates_starting_point = 10000
+    _candidates_starting_point = 31
 
     _voting_table_name = 'voting'
 
