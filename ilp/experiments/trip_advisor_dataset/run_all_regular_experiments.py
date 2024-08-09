f06dc18ddb7f9ee33e9bc1e5752f9112e27831e4
Roi-Yona <roiyona18@gmail.com>
2024-08-09 17:55:57 +0300

    Update trip advisor experiments runner

--- ilp/experiments/trip_advisor_dataset/run_all_regular_experiments.py
+++ ilp/experiments/trip_advisor_dataset/run_all_regular_experiments.py
@@ -2,23 +2,23 @@ import subprocess
 
 # List of scripts to run
 scripts = [
-    ('experiment_1', 'no_constraints_committee_30.py'),
-    ('experiment_2', 'one_dc_committee_30.py'),
-    ('experiment_3', 'one_tgd_committee_30.py'),
-    ('experiment_4', 'one_dc_one_tgd_committee_30.py'),
+    ('experiment_1', 'no_constraints.py'),
+    ('experiment_2', 'one_dc.py'),
+    ('experiment_3', 'one_tgd.py'),
+    ('experiment_4', 'one_dc_one_tgd.py'),
     ('experiment_5', 'one_dc_one_tgd_committee_10.py'),
     ('experiment_6', 'one_dc_one_tgd_committee_20.py'),
     ('experiment_7', 'one_dc_one_tgd_committee_30.py'),
     ('experiment_8', 'one_dc_one_tgd_committee_40.py'),
     ('experiment_9', 'one_dc_one_tgd_committee_50.py'),
-    ('experiment_10', 'one_dc_one_tgd_committee_15_av.py'),
-    ('experiment_11', 'one_dc_one_tgd_committee_15_pav.py'),
-    ('experiment_12', 'one_dc_one_tgd_committee_15_2_truncated_av.py'),
-    ('experiment_13', 'one_dc_one_tgd_committee_15_cc.py'),
-    ('experiment_14', 'one_dc_one_tgd_committee_15_sav.py'),
-    ('experiment_15', 'one_dc_one_tgd_committee_30_2_opt.py'),
-    ('experiment_16', 'one_dc_one_tgd_committee_30_1_opt.py'),
-    ('experiment_17', 'one_dc_one_tgd_committee_30_0_opt.py'),
+    ('experiment_10', 'one_dc_one_tgd_av.py'),
+    ('experiment_11', 'one_dc_one_tgd_pav.py'),
+    ('experiment_12', 'one_dc_one_tgd_2_truncated_av.py'),
+    ('experiment_13', 'one_dc_one_tgd_cc.py'),
+    ('experiment_14', 'one_dc_one_tgd_sav.py'),
+    ('experiment_15', 'one_dc_one_tgd_2_opt.py'),
+    ('experiment_16', 'one_dc_one_tgd_1_opt.py'),
+    ('experiment_17', 'one_dc_one_tgd_0_opt.py'),
 ]
 
 # Run each script with the updated global variable
