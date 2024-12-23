9d91cc960eeab20094f9333094d9320c905cce3a
jonathan <58486928+jonathanBreitman@users.noreply.github.com>
2024-12-24 00:04:15 +0200

    extracted fake progress to a utility and added progress also to the mip conversion

--- mip/mip_db_data_extractors/progress_bar_utils.py
+++ mip/mip_db_data_extractors/progress_bar_utils.py
@@ -1,6 +1,15 @@
+from threading import Thread
 from time import sleep
+from typing import Callable
 
 import streamlit as st
+from matplotlib.pyplot import barbs
+# The next import is for adding the context for threads under streamlit.
+# Note that IT IS NOT a part of the official API, and this currently works for version 1.40.1,
+# See the Github discussion for the history of solution hacks through different versions:
+# https://github.com/streamlit/streamlit/issues/1326
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+
 
 def advance_progress_bar(
         progress_bar: st.delta_generator.DeltaGenerator,
@@ -25,3 +34,31 @@ def advance_progress_bar(
         progress_bar.progress(new_bar_value, progress_text)
 
     sleep(delta_after_loop)
+
+
+def run_func_with_fake_progress_bar(
+        delay: int,
+        loading_message: str,
+        finish_message: str,
+        func_to_run: Callable,
+        *args,
+        **kwargs
+) -> st.delta_generator.DeltaGenerator:
+    progress_bar = st.progress(0, text=loading_message)
+
+    bar_advancement_thread = Thread(
+        target=advance_progress_bar,
+        args=(
+            progress_bar,
+            loading_message,
+            delay
+        )
+    )
+    add_script_run_ctx(bar_advancement_thread)
+
+    bar_advancement_thread.start()
+    func_to_run(*args, **kwargs)
+    bar_advancement_thread.join()
+
+    progress_bar.progress(100, text=finish_message)
+    return progress_bar
