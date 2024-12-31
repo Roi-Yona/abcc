469cbdd13568b51b45e41e7e8efbbfa9fb79b774
jonathan <58486928+jonathanBreitman@users.noreply.github.com>
2024-12-31 10:18:53 +0200

    removed progress bar from mip solver (for now)

--- mip/mip_db_data_extractors/progress_bar_utils.py
+++ mip/mip_db_data_extractors/progress_bar_utils.py
@@ -20,7 +20,7 @@ def advance_progress_bar(
         advance_delay: float = 0.1
 ):
     advances_number = int(delay_time // advance_delay)
-    delta_after_loop = delay_time - (advances_number * advance_delay)
+    delay_left_after_loop = delay_time - (advances_number * advance_delay)
     bar_delta_value = (last_value - starting_value) / advances_number
 
     for i in range(advances_number):
@@ -29,7 +29,7 @@ def advance_progress_bar(
         new_bar_value = starting_value + int(i * bar_delta_value)
         progress_bar.progress(new_bar_value, progress_text)
 
-    sleep(delta_after_loop)
+    sleep(delay_left_after_loop)
 
 
 def run_func_with_fake_progress_bar(
